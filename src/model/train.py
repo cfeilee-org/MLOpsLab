2aed97d17615aa1cd8b03147ce2769edc985f7e5
chinfeilee <chinfeilee@microsoft.com>
2023-08-15 15:29:23 +0800

    fixed code quality #2

--- src/model/train.py
+++ src/model/train.py
@@ -40,7 +40,8 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness',
+               'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
     len(X)
     print(np.unique(y, return_counts=True))
     return train_test_split(X, y, test_size=0.30, random_state=0)
